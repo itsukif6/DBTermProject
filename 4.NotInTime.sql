de27aea0b1349cca12cdaa77c725d2991c4e958c
2**10 <116153038+itsukif6@users.noreply.github.com>
2023-06-10 15:49:33 +0800

    Update 4.NotInTime.sql

--- 4.NotInTime.sql
+++ 4.NotInTime.sql
@@ -1,3 +1,4 @@
-SELECT p.package_id, p.tracking_number
-FROM packages p
-WHERE p.delivery_status = 'Delayed';
+SELECT p.ID, p.Date, p.Sender, p.Receiver
+FROM package p
+JOIN transportcompany t ON p.TrackNumber= t.TrackNumber
+WHERE p.Date > DATE_ADD(t.PackageDate, INTERVAL t.ShippingDays DAY);
