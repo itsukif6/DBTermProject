4fff91d8a019ff01e3283ee003b274ee1d4a28de
2**10 <116153038+itsukif6@users.noreply.github.com>
2023-06-10 15:49:24 +0800

    Update 3.OutOfStock.sql

--- 3.OutOfStock.sql
+++ 3.OutOfStock.sql
@@ -1,7 +1,6 @@
-SELECT p.product_id, p.product_name
-FROM products p
-JOIN inventory i ON p.product_id = i.product_id
-JOIN stores s ON i.store_id = s.store_id
-WHERE s.city = 'Kaohsiung'
-GROUP BY p.product_id, p.product_name
-HAVING COUNT(s.store_id) = (SELECT COUNT(*) FROM stores WHERE city = 'Kaohsiung');
+SELECT p.Name
+FROM Product p
+JOIN Shop s ON p.ShopID = s.ID
+WHERE s.Address = '高雄'
+GROUP BY p.Name
+HAVING SUM(p.Inventory) = 0;
