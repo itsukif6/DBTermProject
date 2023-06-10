67175e4d8ddf85b36a385b45415bc1513e9f71a7
2**10 <116153038+itsukif6@users.noreply.github.com>
2023-06-10 15:49:42 +0800

    Update 5.Replace.sql

--- 5.Replace.sql
+++ 5.Replace.sql
@@ -1,18 +1,9 @@
--- 找到客戶聯絡資訊
-SELECT c.customer_id, c.customer_name, c.email, c.phone
-FROM customers c
-JOIN orders o ON c.customer_id = o.customer_id
-JOIN packages p ON o.order_id = p.order_id
-WHERE p.tracking_number = '123456';
+SELECT c.Name, c.Address
+FROM Customers c
+JOIN PurchaseRecords p ON c.ID = p.CustomerID
+JOIN Package pack ON p.PackageNumber = pack.ID
+WHERE pack.TrackNumber = '123456';
 
--- 找到該包裹的內容
-SELECT p.product_id, p.product_name, p.quantity
-FROM package_contents p
-JOIN packages pkg ON p.package_id = pkg.package_id
-WHERE pkg.tracking_number = '123456';
-
--- 創建新的替換物品運送
-INSERT INTO packages (tracking_number, order_id, delivery_status, shipper_id, tracking_number)
-VALUES ('NEW_TRACKING_NUMBER', 'ORDER_ID', 'Pending', 'SHIPPER_ID', 'NEW_TRACKING_NUMBER');
-INSERT INTO package_contents (package_id, product_id, quantity)
-VALUES ('NEW_PACKAGE_ID', 'PRODUCT_ID', 'QUANTITY');
+-- 創建新的替代貨物發貨記錄
+INSERT INTO PurchaseRecords (ID, Date, CustomerID, Amount, RecordsProductID, Quantity, PackageNumber)
+VALUES (ID, CURDATE(), 1, Amount, RecordsProductID, Quantity, PackageNumber);
